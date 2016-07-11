3f1181519c0f13c83d14f7a2700b76c1986a67a7
bobopinna <bobo@di.unipmn.it>
2016-07-11 11:03:54 +0200

    Added Global Search support

---
+++ classes/search/activity.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Search area for mod_reservation activities.
+ *
+ * @package    mod_reservation
+ * @copyright  2016 Roberto Pinna 
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_reservation\search;
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Reservation activities search area.
+ *
+ * @package    mod_reservation
+ * @copyright  2016 Roberto Pinna
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class activity extends \core_search\area\base_activity {
+}
