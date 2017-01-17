76e3c5d3a9e2bf21351f72e6013f0a13bf560e6f
bobopinna <bobo@di.unipmn.it>
2017-01-17 09:30:58 +0100

    Removed require_once of a missing file

--- backup/moodle2/backup_reservation_activity_task.class.php
+++ backup/moodle2/backup_reservation_activity_task.class.php
@@ -24,7 +24,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 require_once($CFG->dirroot . '/mod/reservation/backup/moodle2/backup_reservation_stepslib.php');
-require_once($CFG->dirroot . '/mod/reservation/backup/moodle2/backup_reservation_settingslib.php');
 
 /**
  * reservation backup task that provides all the settings and steps to perform one
