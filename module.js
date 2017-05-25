e3c9c74d56c91c7e9f317be01816cc68054fc49f
Roberto Pinna <bobo@di.unipmn.it>
2017-05-25 09:47:57 +0200

    removed unused parameter

--- module.js
+++ module.js
@@ -1,13 +1,13 @@
 M.modReservation = {};
 
 M.modReservation.initView = function(Y) {
-    Y.on('click', function(e) {
+    Y.on('click', function() {
         Y.all('input.request').each(function() {
             this.set('checked', 'checked');
         });
     }, '#checkall');
 
-    Y.on('click', function(e) {
+    Y.on('click', function() {
         Y.all('input.request').each(function() {
             this.set('checked', '');
         });
